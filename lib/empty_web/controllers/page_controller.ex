5e72e2fd365248b6dd92eaba9b8317c11785b607
Manoj Samanta <manoj.samanta@gmail.com>
2023-12-01 15:45:43 +0000

    added all info box

--- lib/empty_web/controllers/page_controller.ex
+++ lib/empty_web/controllers/page_controller.ex
@@ -16,6 +16,22 @@ defmodule EmptyWeb.PageController do
              %{class: "bg-warning", icon: "fa-copy", text: "Uploads", number: "13,648"}, 
              %{class: "bg-danger", icon: "fa-star", text: "Likes", number: "93,139"}
             ]
-    render(conn, "widgets.html",  info1: info1, layout: {EmptyWeb.LayoutView, "admin.html"})
+
+    info3 = [
+             %{class: "bg-info", icon: "fa-bookmark", text: "Bookmarks", number: "41,410", progress: "70% Increase in 30 Days" }, 
+             %{class: "bg-success", icon: "fa-thumbs-up", text: "Likes", number: "41,410", progress: "70% Increase in 30 Days" }, 
+             %{class: "bg-warning", icon: "fa-calendar-alt", text: "Events", number: "41,410", progress: "70% Increase in 30 Days" }, 
+             %{class: "bg-danger", icon: "fa-comments", text: "Comments", number: "41,410", progress: "70% Increase in 30 Days" }
+            ]
+
+    info4 = [
+             %{class: "bg-gradient-info", icon: "fa-bookmark", text: "Bookmarks", number: "41,410", progress: "70% Increase in 30 Days" }, 
+             %{class: "bg-gradient-success", icon: "fa-thumbs-up", text: "Likes", number: "41,410", progress: "70% Increase in 30 Days" }, 
+             %{class: "bg-gradient-warning", icon: "fa-calendar-alt", text: "Events", number: "41,410", progress: "70% Increase in 30 Days" }, 
+             %{class: "bg-gradient-danger", icon: "fa-comments", text: "Comments", number: "41,410", progress: "70% Increase in 30 Days" }
+            ]
+
+
+    render(conn, "widgets.html",  info1: info1, info3: info3, info4: info4, layout: {EmptyWeb.LayoutView, "admin.html"})
   end
 end
--- lib/empty_web/views/page_view.ex
+++ lib/empty_web/views/page_view.ex
@@ -15,23 +15,9 @@ acc <> "<div class=\"col-md-3 col-sm-6 col-12\">\n <div class=\"info-box shadow-
    |> raw
   end
 
-  def info3s(x) do
+  def infobox3(x) do
    Enum.reduce(x, "", fn (n, acc) -> 
-acc <> "<div class=\"col-md-3 col-sm-6 col-12\">\n <div class=\"info-box\">\n <span class=\"info-box-icon "<> n[:class] <>"\"><i class=\"far " <> n[:icon] <>"\"></i></span>\n <div class=\"info-box-content\">\n <span class=\"info-box-text\">" <> n[:text] <>"</span>\n <span class=\"info-box-number\">" <> n[:number] <> "</span>\n </div>\n </div>\n </div>\n"
-    end) 
-   |> raw
-  end
-
-  def info4s(x) do
-   Enum.reduce(x, "", fn (n, acc) -> 
-acc <> "<div class=\"col-md-3 col-sm-6 col-12\">\n <div class=\"info-box\">\n <span class=\"info-box-icon "<> n[:class] <>"\"><i class=\"far " <> n[:icon] <>"\"></i></span>\n <div class=\"info-box-content\">\n <span class=\"info-box-text\">" <> n[:text] <>"</span>\n <span class=\"info-box-number\">" <> n[:number] <> "</span>\n </div>\n </div>\n </div>\n"
-    end) 
-   |> raw
-  end
-
-  def info5s(x) do
-   Enum.reduce(x, "", fn (n, acc) -> 
-acc <> "<div class=\"col-md-3 col-sm-6 col-12\">\n <div class=\"info-box\">\n <span class=\"info-box-icon "<> n[:class] <>"\"><i class=\"far " <> n[:icon] <>"\"></i></span>\n <div class=\"info-box-content\">\n <span class=\"info-box-text\">" <> n[:text] <>"</span>\n <span class=\"info-box-number\">" <> n[:number] <> "</span>\n </div>\n </div>\n </div>\n"
+acc <> "<div class=\"col-md-3 col-sm-6 col-12\">\n <div class=\"info-box " <> n[:class] <> "\">\n <span class=\"info-box-icon\"><i class=\"far " <> n[:icon] <>"\"></i></span>\n <div class=\"info-box-content\">\n <span class=\"info-box-text\">" <> n[:text] <>"</span>\n <span class=\"info-box-number\">" <> n[:number] <> "</span>\n <div class=\"progress\">\n <div class=\"progress-bar\" style=\"width: 70%\"></div>\n </div>\n <span class=\"progress-description\">\n" <> n[:progress] <> "</span>\n</div>\n </div>\n </div>\n"
     end) 
    |> raw
   end
